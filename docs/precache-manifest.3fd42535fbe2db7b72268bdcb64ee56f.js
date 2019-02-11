3e6c4f3d8ab0856eabf60390d7024276d807dee6
Christian Terence <chrisc98@vt.edu>
2019-02-10 21:12:19 -0500

    Web

---
+++ docs/precache-manifest.3fd42535fbe2db7b72268bdcb64ee56f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "306b1fc42f453c2421da33febd95820e",
+    "url": "/static/media/braven.306b1fc4.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bbe1c041336db743758d",
+    "url": "/static/js/main.bbe1c041.chunk.js"
+  },
+  {
+    "revision": "b9758cb22a500ae716ee",
+    "url": "/static/js/1.b9758cb2.chunk.js"
+  },
+  {
+    "revision": "bbe1c041336db743758d",
+    "url": "/static/css/main.60e517a1.chunk.css"
+  },
+  {
+    "revision": "ad81eba9830d8e9a0d8247dbb70782f1",
+    "url": "/index.html"
+  }
+];
