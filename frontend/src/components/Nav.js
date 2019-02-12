174176cd65ede6b82a9925d1283591de2bde9bc5
Braxenz <34330584+Braxenz@users.noreply.github.com>
2019-02-12 11:43:22 -0500

    Braven

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -2,17 +2,42 @@ import React from 'react';
 import './Nav.css';
 
 class Nav extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            hidden: true,
+        }
+    }
+    toggleNav = () => {
+        this.setState({
+            hidden: false,
+        })
+    }
+    closeNav = () => {
+        this.setState({
+            hidden: true,
+        })
+    }
+
     render() {
+        const { hidden } = this.state;
         return (
-            <div className="navbar-container">
-                <div className="home-button">
-                    Home
-                </div>
-                <div className="contact-button">
-                    Contact
-                </div>
-                
-            </div>
+            <React.Fragment>
+                {!hidden && (
+                    <div className="navbar-container">
+                        <div className="home-button">Home</div>
+                        <div className="contact-button">Contact</div>
+                        <div onClick={this.closeNav} className="home-button">Close</div>
+                    </div>
+                )}
+
+                {hidden && (
+                    <div onClick={this.toggleNav} className="slider">
+                        >
+                    </div>
+                )}
+            </React.Fragment>
         )
     }
 }
--- frontend/src/pages/homepage/homepage.js
+++ frontend/src/pages/homepage/homepage.js
@@ -4,21 +4,21 @@ import Nav from '../../components/Nav';
 import { Link } from 'react-router-dom';
 
 class HomePage extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+    constructor(props) {
+        super(props);
+    }
 
 
-  render() {
-    return (
-     <div className="app-container">
-        <Nav/>
-        <div className="home-body">
-            Welcome!
-        </div>
-     </div>
-    )
-  }
+    render() {
+        return (
+            <div className="app-container">
+                <Nav />
+                <div className="home-body">
+                    Welcome!
+                </div>
+            </div>
+        )
+    }
 }
 
 export default HomePage;
